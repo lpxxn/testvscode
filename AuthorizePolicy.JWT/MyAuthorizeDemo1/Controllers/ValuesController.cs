9587f7242ae6f3071496948324e11c298cda96ba
lpxxn <mi_duo@live.com>
2017-12-22 14:04:39 +0800

    test  token

---
+++ AuthorizePolicy.JWT/MyAuthorizeDemo1/Controllers/ValuesController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace MyAuthorizeDemo1.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [AllowAnonymous]
+        // POST api/values
+        [HttpPost]
+        public string Post([FromBody]string value)
+        {
+            //This method returns user id from username and password.
+            var userId = value;
+            if (string.IsNullOrEmpty(userId))
+            {
+                return "bad";
+            }
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userId),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken
+            (
+                issuer: "aa",//_configuration["Issuer"],
+                audience: "bb",//_configuration["Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(60),
+                notBefore: DateTime.UtcNow,
+                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes("cccccccccccccccccccccccccccccccccccccccccccccccc")),
+                    SecurityAlgorithms.HmacSha256)
+            );
+
+            //return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+            var strToken = new JwtSecurityTokenHandler().WriteToken(token);
+            return strToken;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ AuthorizePolicy.JWT/MyAuthorizeDemo1/Startup.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+
+namespace MyAuthorizeDemo1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(jwtBearerOptions =>
+                {
+                    jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateActor = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = "aa",//Configuration["Issuer"],
+                        ValidAudience = "bb", //Configuration["Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("cccccccccccccccccccccccccccccccccccccccccccccccc"))
+                        //IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["SigningKey"]))
+                        
+                    };
+                    
+//                       jwtBearerOptions.Events = new JwtBearerEvents()
+//                       {
+//                           OnAuthenticationFailed = ctx =>
+//                           {
+//                               var obj1 = new {Name = "li李", Age = 12};
+//                               var js1 = JsonConvert.SerializeObject(obj1);
+//                               using (MemoryStream stream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(js1)))
+//                               {
+//                                   ctx.Response.Body = stream;
+//                               }
+                               
+                         //      return Task.CompletedTask;
+                          // }
+                //       };
+                });
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
